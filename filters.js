8a3c5b754b4ab907f029cc813dc221da01a9587d
pavanijl <59906774+pavanijl@users.noreply.github.com>
2021-03-10 20:47:37 -0600

    Add files via upload

---
+++ filters.js
@@ -0,0 +1,218 @@
+function clickSpatial(){
+  $(document).on('click', '#spatial', function (e) {
+    $(".main_display").hide();
+    $("#myImg1, #myImg20, #myImg42, #myImg98, #myImg3, #myImg21, #myImg8, #myImg27, #myImg38, #myImg65, #myImg31, #myImg30, #myImg115, #myImg77, #myImg114, #myImg49, #myImg110, #myImg56, #myImg86, #myImg117, #myImg51,#myImg32,#myImg94,#myImg73,#myImg105,#myImg106,#myImg25, #myImg58, #myImg24,#myImg93,#myImg91,#myImg74,#myImg121,#myImg59,#myImg123,#myImg104, #myImg57,#myImg91,#myImg23,#myImg124,#myImg64,#myImg2,#myImg111,#myImg99,#myImg116,#myImg109,#myImg113,#myImg55,#myImg67,#myImg107,#myImg108,#myImg54,#myImg61,#myImg95,#myImg38,#myImg20,#myImg100,#myImg87,#myImg78,#myImg76,#myImg25,#myImg83,#myImg97,#myImg75").show();
+    e.preventDefault();
+});
+}
+
+function clickTemporal(){
+  $(document).on('click', '#temporal', function (e) {
+    // $(".main_display").hide();
+    // $("#myImg4, #myImg5, #myImg6, #myImg7").show();
+    e.preventDefault();
+});
+}
+function clickSpatialTemporal(){
+
+  $(document).on('click', '#spatioTemporal', function (e) {
+    // $(".main_display").hide();
+    // $("#myImg4, #myImg5, #myImg6, #myImg7").show();
+    e.preventDefault();
+});
+}
+function clickVisualization(){
+
+  $(document).on('click', '#visualization', function (e) {
+    // $(".main_display").hide();
+    // $("#myImg4, #myImg5, #myImg6, #myImg7").show();
+    e.preventDefault();
+});
+}
+function clickOtherMap(){
+
+  $(document).on('click', '#otherMap', function (e) {
+    $(".main_display").hide();
+    $("#myImg31, #myImg30, #myImg115, #myImg77, #myImg114, #myImg117,#myImg51, #myImg94,#myImg105,#myImg58,#myImg24,#myImg121, #myImg111, #myImg116,#myImg109,#myImg107,#myImg25").show();
+    e.preventDefault();
+});
+}
+function clickCartographicMap(){
+
+  $(document).on('click', '#cartographicMap', function (e) {
+    $(".main_display").hide();
+    $("#myImg1, #myImg20, #myImg42, #myImg98, #myImg3, #myImg21, #myImg8, #myImg27, #myImg38, #myImg49, #myImg110, #myImg56, #myImg86,#myImg51,#myImg32,#myImg94,#myImg73,#myImg25,#myImg93,#myImg91, #myImg74,#myImg59,#myImg123,#myImg104,#myImg57,#myImg91,#myImg23,#myImg124,#myImg2,#myImg99,#myImg113,#myImg55,#myImg67,#myImg108,#myImg54,#myImg61,#myImg95,#myImg38,#myImg20, #myImg100,#myImg87,#myImg78,#myImg76,#myImg83,#myImg75").show();
+    e.preventDefault();
+});
+}
+function clickScatterPlot(){
+
+  $(document).on('click', '#scatterPlot', function (e) {
+    $(".main_display").hide();
+    $("#myImg115, #myImg64").show();
+    e.preventDefault();
+});
+}
+function clickCircularChart(){
+
+  $(document).on('click', '#circularChart', function (e) {
+    $(".main_display").hide();
+    $("#myImg65,#myImg106").show();
+    e.preventDefault();
+});
+}
+function clickTimeLine(){
+
+  $(document).on('click', '#timeline', function (e) {
+    // $(".main_display").hide();
+    // $("#myImg4, #myImg5, #myImg6, #myImg7").show();
+    e.preventDefault();
+});
+}
+function clickTimeCurve(){
+
+  $(document).on('click', '#timecurve', function (e) {
+    // $(".main_display").hide();
+    // $("#myImg4, #myImg5, #myImg6, #myImg7").show();
+    e.preventDefault();
+});
+}
+function clickTimeFrames(){
+
+  $(document).on('click', '#timeframes', function (e) {
+    // $(".main_display").hide();
+    // $("#myImg4, #myImg5, #myImg6, #myImg7").show();
+    e.preventDefault();
+});
+}
+function clickTimeCircle(){
+
+  $(document).on('click', '#timecircle', function (e) {
+    // $(".main_display").hide();
+    // $("#myImg4, #myImg5, #myImg6, #myImg7").show();
+    e.preventDefault();
+});
+}
+function clickTimeTable(){
+
+  $(document).on('click', '#timetable', function (e) {
+    // $(".main_display").hide();
+    // $("#myImg4, #myImg5, #myImg6, #myImg7").show();
+    e.preventDefault();
+});
+}
+function clickColor(){
+
+  $(document).on('click', '#color', function (e) {
+    $(".main_display").hide();
+    $("#myImg65, #myImg115, #myImg117,#myImg104,#myImg2,#myImg111").show();
+    e.preventDefault();
+});
+}
+function clickPosition(){
+
+  $(document).on('click', '#position', function (e) {
+    $(".main_display").hide();
+    $("#myImg1, #myImg20, #myImg42, #myImg98, #myImg3, #myImg21, #myImg8, #myImg27, #myImg38, #myImg31, #myImg30, #myImg115, #myImg77, #myImg114, #myImg49, #myImg110, #myImg56, #myImg86,#myImg117,#myImg51,#myImg32,#myImg94,#myImg73,#myImg105,#myImg106,#myImg25,#myImg58,#myImg24,#myImg93,#myImg91,#myImg74,#myImg121,#myImg59,#myImg123,#myImg104,#myImg57,#myImg91,#myImg23,#myImg124,#myImg64, #myImg2,#myImg111,#myImg99,#myImg116,#myImg109,#myImg113,#myImg55,#myImg67,#myImg107,#myImg108,#myImg54,#myImg61,#myImg95, #myImg38,#myImg20,#myImg100,#myImg87,#myImg78,#myImg76,#myImg25").show();
+    e.preventDefault();
+});
+}
+function clickOrientation(){
+
+  $(document).on('click', '#orientation', function (e) {
+    $(".main_display").hide();
+    $("#myImg65,#myImg106").show();
+    e.preventDefault();
+});
+}
+function clickText(){
+
+  $(document).on('click', '#text', function (e) {
+    $(".main_display").hide();
+    $("#myImg8").show();
+    e.preventDefault();
+});
+}
+
+
+
+function clickInteraction(){
+
+  $(document).on('click', '#interaction', function (e) {
+    // $(".main_display").hide();
+    // $("#myImg4, #myImg5, #myImg6, #myImg7").show();
+    e.preventDefault();
+});
+}
+function clickCoordinates(){
+
+  $(document).on('click', '#coordinates', function (e) {
+    // $(".main_display").hide();
+    // $("#myImg4, #myImg5, #myImg6, #myImg7").show();
+    e.preventDefault();
+});
+}
+function clickLayers(){
+
+  $(document).on('click', '#layers', function (e) {
+    // $(".main_display").hide();
+    // $("#myImg4, #myImg5, #myImg6, #myImg7").show();
+    e.preventDefault();
+});
+}
+function clickAnimation(){
+
+  $(document).on('click', '#animation', function (e) {
+    // $(".main_display").hide();
+    // $("#myImg4, #myImg5, #myImg6, #myImg7").show();
+    e.preventDefault();
+});
+}
+function clickSeperate(){
+
+  $(document).on('click', '#seperate', function (e) {
+    // $(".main_display").hide();
+    // $("#myImg4, #myImg5, #myImg6, #myImg7").show();
+    e.preventDefault();
+});
+}
+function clickVisual(){
+
+  $(document).on('click', '#visual', function (e) {
+    // $(".main_display").hide();
+    // $("#myImg4, #myImg5, #myImg6, #myImg7").show();
+    e.preventDefault();
+});
+}
+function click2D(){
+
+  $(document).on('click', '#2d', function (e) {
+    // $(".main_display").hide();
+    // $("#myImg4, #myImg5, #myImg6, #myImg7").show();
+    e.preventDefault();
+});
+}
+function click3D(){
+
+  $(document).on('click', '#3d', function (e) {
+    // $(".main_display").hide();
+    // $("#myImg4, #myImg5, #myImg6, #myImg7").show();
+    e.preventDefault();
+});
+}
+function clickSingleView(){
+
+  $(document).on('click', '#singleView', function (e) {
+    // $(".main_display").hide();
+    // $("#myImg4, #myImg5, #myImg6, #myImg7").show();
+    e.preventDefault();
+});
+}
+function clickMultipleView(){
+
+  $(document).on('click', '#multipleView', function (e) {
+    // $(".main_display").hide();
+    // $("#myImg4, #myImg5, #myImg6, #myImg7").show();
+    e.preventDefault();
+});
+}
